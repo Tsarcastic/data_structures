b30e8fcee446260b7ac3a55ab90f9930e663ca38
Brendan Davis <brendanmd@gmail.com>
2017-12-09 19:07:35 -0800

    laptop to desktop

--- src/bubble_sort.py
+++ src/bubble_sort.py
@@ -1,12 +1,17 @@
 """Sort with bubbles."""
-
+import timeit
 
 def bubble_sort(nums):
     """Sort those numbers up."""
     i = 0
     length = len(nums)
-    for i in range(length -1):
+    for i in range(length - 1):
             for i in range(length - 1):
                 if nums[i] > nums[i + 1]:
                     nums[i], nums[i + 1] = nums[i + 1], nums[i]
     return nums
+
+if __name__ == '__main__':
+    start_time = timeit.default_timer()
+    print(bubble_sort(range(1, 10000, 500)))
+    print(timeit.default_timer() - start_time)
