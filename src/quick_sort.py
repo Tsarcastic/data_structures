b5ad617bd82990e0f8042ce48e3fbb5b12e11180
Brendan Davis <BrendanMD@gmail.com>
2018-01-02 10:18:23 -0800

    Length 1 and 2 functionality added.

--- src/quick_sort.py
+++ src/quick_sort.py
@@ -3,10 +3,14 @@
 def quick_sort(nums):
     """Standard quick sort."""
     the_length = len(nums)
+    low_list = []
+    high_list = []
     if the_length == 1:
-        pass
+        return nums
     elif the_length == 2:
-        pass
+        if nums[1] < nums[0]:
+            nums[0], nums[1] = nums[1], nums[0]
+        return nums
     elif the_length == 3:
         pass
-    else
+    else:
