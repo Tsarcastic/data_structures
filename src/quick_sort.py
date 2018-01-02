39b59126e549dc3fcec50775434688bdd4030014
Brendan Davis <BrendanMD@gmail.com>
2018-01-01 22:19:05 -0800

    just added a few lines

---
+++ src/quick_sort.py
@@ -0,0 +1,12 @@
+"""A partition-exchange sort."""
+
+def quick_sort(nums):
+    """Standard quick sort."""
+    the_length = len(nums)
+    if the_length == 1:
+        pass
+    elif the_length == 2:
+        pass
+    elif the_length == 3:
+        pass
+    else
