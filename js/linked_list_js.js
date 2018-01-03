b5d01c8bb20a1a564e29ac490460eabf8abd6dad
Brendan Davis <BrendanMD@gmail.com>
2018-01-03 14:55:17 -0800

    Set up classes for Node & Linked List.

--- js/linked_list_js.js
+++ js/linked_list_js.js
@@ -1,1 +1,22 @@
+'use strict'
 
+class Node {
+    constructor(data) {
+        this.data = data
+        this.next = null
+    }
+}
+
+
+class LinkedList {
+    constructor() {
+        this.head = null
+        this._counter = 0
+    }
+
+    size() {
+        return this._counter
+    }
+
+    
+}
