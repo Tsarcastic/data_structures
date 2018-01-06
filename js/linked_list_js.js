ddcc34fddc17c7f14a6cdf1e01c0e097ee3f1e9f
Brendan Davis <BrendanMD@gmail.com>
2018-01-05 18:34:14 -0800

    Methods are included.

--- js/linked_list_js.js
+++ js/linked_list_js.js
@@ -1,5 +1,5 @@
 /*jshint esversion: 6 */
-//'use strict()'
+'use strict()'
 
 
 var sum = require('./sum')
@@ -26,9 +26,61 @@ class LinkedList {
         this.head = new Node(val, this.head)
         this._counter += 1
     }
+
+    pop() {
+        if(this.head == null) {
+            throw 'The list is empty.'
+        }
+        var output = this.head.data
+        this.head = this.head.next 
+        this._counter += -1
+        return output
+    }
+
+    search(val) {
+        var curr = this.head 
+        if(curr === null) {
+            throw 'The list is empty.'
+        }
+        while (curr != null) {
+            if (curr.data == val) {
+                return curr
+            }
+        }
+        curr = curr.next
+
+    }
+
+    remove(val) {
+        var curr = this.head
+        if(curr == null) {
+            throw 'The list is empty.'
+        }
+        while (curr != null) {
+            if(curr.next.data == val) {
+                curr.next = curr.next.next
+                self._counter += -1
+                return
+            }
+            curr = curr.next
+        }
+
+
+    }
+
+    display() {
+        if (self._counter == 0) {
+            throw 'The list is empty.'
+        }
+        var curr = this.head
+        var the_answer = []
+        while (curr != null) {
+            the_answer.push(curr.data)
+            curr = curr.next
+        }
+        return the_answer.join(',')
+    }
+
 }
 
-
-
-
-//module.exports = {LinkedList, Node}
+module.exports = {LinkedList, Node}
