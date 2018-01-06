651c61e02e71a4431df7ba16e43d29decf49533c
Brendan Davis <BrendanMD@gmail.com>
2018-01-06 08:01:12 -0800

    Rough draft of binheap is up.

--- js/binheap_js.js
+++ js/binheap_js.js
@@ -27,7 +27,38 @@ class Binheap {
                 index = floor(index / 2)
             }
             range += -1
-        } 
+        }
+    }
+
+    sort_down() {
+        var index = 1
+        while(index * 2 <= this.heap_index) {
+            var child_idx = index * 2
+            if((index * 2) + 1 <= this.heap_index) {
+                if(this.bin_list[index * 2] > this.bin_list[(index * 2) + 1]) {
+                    child_idx = (index * 2) + 1
+                } else {
+                    child_idx = index * 2
+                }
+            } else {
+                child_idx = index * 2
+            }
+            var parent = this.bin_list[index]
+            if(this.bin_list[index] > this.bin_list[child_idx]) {
+                this.bin_list[index] = this.bin_list[child_idx]
+                this.bin_list[child_idx] = parent
+            }
+            index = child_idx
+        }
+    }
+
+    pop() {
+        var to_remove = this.bin_list[1]
+        this.bin_list[1] = this.bin_list[-1]
+        this.bin_list = this.bin_list.slice(0, -1)
+        this.heap_index += -1
+        this.sort_down()
+        return to_remove
     }
 }
 
